4fc2c6d33c6cd409bcca91cd1d5f69a0f8591ad4
Ivanok <ivan243117@gmail.com>
2025-11-06 21:13:52 -0500

    Función con parámetros y sin retorno

---
+++ PrintSum.java
@@ -0,0 +1,10 @@
+public class PrintSum {
+    public static void main(String[] args) {
+        mostrarSuma(19, 12);
+    }
+
+    public static void mostrarSuma(int a, int b) {
+        int suma = a + b;
+        System.out.println("La suma es: " + suma);
+    }
+}
