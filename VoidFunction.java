64ff3a7fac04427659d7b816afac0a20c14b9fd4
Ivanok <ivan243117@gmail.com>
2025-11-06 21:09:20 -0500

    Función sin parámetros y sin retorno

---
+++ VoidFunction.java
@@ -0,0 +1,9 @@
+public class VoidFunction {
+    public static void main(String[] args) {
+        saludar(); // Llamamos la función saludar
+    }
+
+    public static void saludar() {
+        System.out.println("¡Hola, mundo!");
+    }
+}
