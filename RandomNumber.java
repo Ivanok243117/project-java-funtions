f78fcb225bcb4d8274a96b98a98ec174cab3c914
Ivanok <ivan243117@gmail.com>
2025-11-06 21:17:48 -0500

    Función sin parámetros y con retorno

---
+++ RandomNumber.java
@@ -0,0 +1,10 @@
+public class RandomNumber {
+    public static void main(String[] args) {
+        int numero = obtenerNumero();
+System.out.println("Número devuelto: " + numero);
+    }
+
+    public static int obtenerNumero() {
+        return 31; // Simulamos que retorna un número fijo
+    }
+}
